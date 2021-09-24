a20bbcdd0a0130d2bdac20f7f2933b8d7a0cb6d6
jj-mac <cjdool@naver.com>
2021-09-24 19:12:03 +0900

    1.3st Version of 1_saxpy(Segmentation Fault Fix)

--- lab1/1_saxpy/saxpy.cu
+++ lab1/1_saxpy/saxpy.cu
@@ -94,7 +94,7 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     double endTime2 = CycleTimer::currentSeconds();
 
     // TODO: copy result from GPU back to CPU using cudaMemcpy
-    cudaMemcpy(device_result, resultarray, totalBytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(device_result, resultarray, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
     // end timing after result has been copied back into host memory
     double endTime = CycleTimer::currentSeconds();
